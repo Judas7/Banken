daed300c83e3244f6db57bbc9230baebfbdaeb8b
limpan.landin@gmail.com <ll0009@ad.lystkom.se>
2020-09-10 10:23:08 +0200

    Add project files.

---
+++ Banken/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banken
+{
+    class Customer
+    {
+        public string Namn { get; set; }
+        public int Balance { get; set; }
+        public string ShowCustomer { get { return "Hej" + Namn + "Saldo: " + Balance; } }
+    }
+}
---
+++ Banken/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banken
+{
+    class Program
+    {
+        static List<Customer> Customers = new List<Customer>();
+        static void Main(string[] args)
+        {
+            SelectMenuItem();
+        }
+
+        private static void SelectMenuItem()
+        {
+            Console.WriteLine("Välkommen till Banken!");
+            Console.WriteLine("");
+            Console.WriteLine("Ange vilket av följande alternativ du vill göra");
+            Console.WriteLine("");
+            Console.WriteLine("1: Lägg till en användare");
+            Console.WriteLine("2: Ta bort en användare");
+            Console.WriteLine("3: Visa alla befintliga användare");
+            Console.WriteLine("4: Visa saldo");
+            Console.WriteLine("5: Gör en insättning");
+            Console.WriteLine("6: Gör ett uttag");
+            Console.WriteLine("7: Avsluta programmet");
+            Console.WriteLine("");
+            Console.WriteLine("Skriv ditt val:");
+        }
+    }
+}
